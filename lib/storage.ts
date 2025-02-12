dd4c849f0e0286f15f3aa544aead6d0fbfcd3888
ido navot private <idonavot303@gmail.com>
2025-02-12 19:10:11 +0200

    fixed duplicates cities in recent searches

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,7 +1,13 @@
-import { City } from "@/types";
+import { City } from '@/types';
 
 export const addToRecentCities = (cityName: string) => {
-  const cities: City[] = [];
+  const existingCities = localStorage.getItem('recentCities');
+  const cities: City[] = existingCities ? JSON.parse(existingCities) : [];
+
+  const cityIndex = cities.findIndex((city) => city.name === cityName);
+  if (cityIndex !== -1) {
+    cities.splice(cityIndex, 1);
+  }
 
   cities.unshift({
     name: cityName,
@@ -11,5 +17,8 @@ export const addToRecentCities = (cityName: string) => {
 
   // Keep only last 5 cities
   const updatedCities = cities.slice(0, 5);
+
+  localStorage.setItem('recentCities', JSON.stringify(updatedCities));
+
   return updatedCities;
 };
